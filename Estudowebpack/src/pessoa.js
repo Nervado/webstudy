f9f5a03ca2e9520b71ae4c7504febbfa33025362
Nervado <evandroabreudeoliveira@gmail.com>
2019-05-18 17:52:21 -0300

    definindo estrutura do projeto

---
+++ Estudowebpack/src/pessoa.js
@@ -0,0 +1,4 @@
+//module.exports
+export default class Pessoa {
+    cumprimentar() { return 'Bom dia !' }
+}
---
+++ Estudowebpack/src/principal.js
@@ -0,0 +1,5 @@
+//const Pessoa = require('./pessoa') // modulos do node
+import Pessoa from './pessoa' // ECMAS padrão
+
+const atendente = new Pessoa
+console.log(atendente.cumprimentar())
