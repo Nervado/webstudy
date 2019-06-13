f9823f7d5c1f2c9b9455b4e403127eff51388fd2
Nervado <evandroabreudeoliveira@gmail.com>
2019-06-13 11:39:13 -0300

    udpate bootstrap

---
+++ estudojquery/js/temporizador.js
@@ -0,0 +1,61 @@
+// Define a IIFE - Immediately Invoked Function Expression
+(function ($) {
+    $.fn.temporizador = function (opcoes) {
+        // O $.extend() merge the contents of two more objects into the firts object
+        const opcoesFinais = $.extend({
+            mensagem: 'Em breve!',
+            horario: '23:59:59'
+        }, opcoes) // pode ser usado o operador spread ...
+
+        // Spans para hora
+        const horaDezena = $('<span class="digito">').html('0')
+        const horaUnidade = $('<span class="digito">').html('0')
+        const minutoDezena = $('<span class="digito">').html('0')
+        const minutoUnidade = $('<span class="digito">').html('0')
+        const segundoDezena = $('<span class="digito">').html('0')
+        const segundoUnidade = $('<span class="digito">').html('0')
+
+
+        // Separadores
+        const separadorHora = $('<span class="separador">').html(':')
+        const separadorMinuto = $('<span class="separador">').html(':')
+
+        // Mensagem
+        const mensagem = $('<div class="mensagem">').html(opcoesFinais.mensagem)
+
+        $(this).addClass('temporizador')
+        $(this).append(horaDezena, horaUnidade, separadorHora, minutoDezena,
+            minutoUnidade, separadorMinuto, segundoDezena, segundoUnidade, mensagem)
+
+        // Cria um regexp para capiturar informação de hora de um obejto data 
+        const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/) //Padrão universal
+        const horarioAlvo = regex.exec(opcoesFinais.horario)
+        
+        let temporizador = setInterval(()=>{ 
+            const agora = new Date()
+            const alvo = new Date()
+            alvo.setHours(horarioAlvo[1])
+            alvo.setMinutes(horarioAlvo[2])
+            alvo.setSeconds(horarioAlvo[3])
+            
+            const diferencaEmMili = alvo.getTime() - agora.getTime()
+            if(diferencaEmMili >=0 ){
+                const diferenca = regex.exec(new Date(diferencaEmMili).toISOString())
+                horaDezena.html(diferenca[1][0])
+                horaUnidade.html(diferenca[1][1])
+   
+                minutoDezena.html(diferenca[2][0])
+                minutoUnidade.html(diferenca[2][1])
+   
+                segundoDezena.html(diferenca[3][0])
+                segundoUnidade.html(diferenca[3][1])
+                
+            }else{
+                clearInterval(temporizador)
+            }
+
+        },1000)
+
+        return this
+    }
+})(jQuery)
