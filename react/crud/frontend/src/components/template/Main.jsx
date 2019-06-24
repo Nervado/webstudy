af5674ec9d6ae89eced808f0447ceb8923fefc20
Nervado <evandroabreudeoliveira@gmail.com>
2019-06-24 07:41:41 -0300

    teste

---
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css'
+import Header from './Header'
+import React from 'react'
+
+export default props =>
+    <React.Fragment>
+        <Header {...props}/>
+        <main className="content">
+            Conteúdo
+        </main>
+    </React.Fragment>
