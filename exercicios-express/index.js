bcdbdc164de877547fbc86fb990dee2b315afe96
Nervado <evandroabreudeoliveira@gmail.com>
2019-07-20 09:15:00 -0300

    files

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,18 @@
 const saudacao = require('./saudacao')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const userApi = require('./api/user')
+
+
+app.post('/usuario', userApi.salvar)
+app.get('/usuario', userApi.obter)
+
+require('./api/product')(app, 'com param!')
+
+app.use(bodyParser.text()) // Midware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(saudacao('Evandro'))
 
@@ -12,17 +24,20 @@ app.get('/clientes/relatorio', (req, res) =>  {
 })
 
 app.post('/corpo',(req,res) => {
-    
+    /* let corpo = ''
+    req.on('data', function(parte){
+        corpo +=parte 
+    })
+    req.on('end', function(){
+        res.send(corpo)
+    }) */
+    res.send(req.body)
 })
-
 
 // :id parte variavel ou parametro
 app.get('/clientes/:id',(req, res)=>{
     res.send(`Cliente ${req.params.id} selecionado!`)
 })
-
-
-
 
 // use,get,all and option to say what we can do
 app.get('/opa',(req, res, next) => {
